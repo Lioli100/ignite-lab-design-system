3afa24464f60b20afeee283588ba75003509c003
Mario Lioli Pacheco <pacheco.lioli@gmail.com>
2022-10-17 12:06:16 -0300

    ci: edit config.base value to my repository name

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -4,9 +4,12 @@ import { Button, ButtonProps } from './Button'
 export default {
   title: "Components/Button",
   component: Button,
+  
   args: {
     children: "Create account"
   }, 
+
+
   argTypes: {}
 } as Meta<ButtonProps>
 
